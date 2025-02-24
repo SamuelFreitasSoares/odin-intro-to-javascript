aa8973c9735e30d85fc6d0089df15b55481a4f6a
SamuelFreitasSoares <samuelfreitassoares@outlook.com>
2025-02-24 14:16:47 -0300

    :sparkles: feat: added exercise #2 about TDD and JavaScript

---
+++ 02_repeatString/repeatString.js
@@ -0,0 +1,13 @@
+const repeatString = function(word, times) {
+    let result="";
+    if(times < 0) {
+        return "ERROR";
+    }
+    for(let i = 0; i < times; i++){
+        result = result + word;
+    }
+    return result;
+};
+
+// Do not edit below this line
+module.exports = repeatString;
